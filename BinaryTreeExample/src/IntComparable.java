973b6a4b37b5629df69e0a81791e0a62aa6d1c43
Vovencio <vladimirkondratyev2@gmail.com>
2026-01-29 22:29:17 +0100

    just a push

---
+++ BinaryTreeExample/src/IntComparable.java
@@ -0,0 +1,24 @@
+import java.util.Objects;
+
+public class IntComparable implements ComparableContent<IntComparable>{
+    Integer wert;
+
+    public IntComparable(Integer wert) {
+        this.wert = wert;
+    }
+
+    @Override
+    public boolean isGreater(IntComparable pContent) {
+        return wert > pContent.wert;
+    }
+
+    @Override
+    public boolean isEqual(IntComparable pContent) {
+        return Objects.equals(wert, pContent.wert);
+    }
+
+    @Override
+    public boolean isLess(IntComparable pContent) {
+        return wert < pContent.wert;
+    }
+}
---
+++ BinaryTreeExample/src/Main.java
@@ -0,0 +1,10 @@
+//TIP To <b>Run</b> code, press <shortcut actionId="Run"/> or
+// click the <icon src="AllIcons.Actions.Execute"/> icon in the gutter.
+void main() {
+    IntComparable a = new IntComparable(10);
+    IntComparable b = new IntComparable(21);
+    IntComparable c = new IntComparable(1);
+    IntComparable d = new IntComparable(-20);
+
+    BinarySearchTree<IntComparable> v = new BinarySearchTree<>();
+}
